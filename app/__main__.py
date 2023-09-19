cea786d85ff877cfba06e5193e60ff230eb180a4
Chris Watkins <chris@watkinslabs.com>
2023-09-19 07:37:21 -0400

    Ading systemd Service and uwsgi

--- app/__main__.py
+++ app/__main__.py
@@ -6,35 +6,31 @@ from flask_login import LoginManager, login_user, login_required, logout_user, c
 from app.config import Config
 from app.extensions import db
 
-def create_app(config_class=Config):
-    app = Flask(__name__)
-    app.config.from_object(config_class)
+app = Flask(__name__)
+app.config.from_object(Config)
 
-    db.init_app(app)
- 
-    from app.models.user import User
-    # Initialize Flask extensions here
+db.init_app(app)
 
-    # Register blueprints here
-    from app.main import bp as main_bp
-    app.register_blueprint(main_bp)
-        
-    from app.auth import bp as auth_bp
-    app.register_blueprint(auth_bp)
+from app.models.user import User
+# Initialize Flask extensions here
 
-    from app.dashboard import bp as dash_bp
-    app.register_blueprint(dash_bp)
+# Register blueprints here
+from app.main import bp as main_bp
+app.register_blueprint(main_bp)
+    
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp)
 
-    login_manager = LoginManager()
-    login_manager.init_app(app)
-    login_manager.login_view = 'auth.login'
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
+from app.dashboard import bp as dash_bp
+app.register_blueprint(dash_bp)
 
-    return app
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'auth.login'
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
 if __name__ == '__main__':
-    app=create_app()
     app.run(debug=True)
